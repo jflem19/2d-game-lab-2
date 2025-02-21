09df90854e38bcd600a19488f3d6d732240d12a1
jflem19 <jfleming19@fordham.edu>
2025-02-21 12:57:31 -0500

    made car

---
+++ lab-2/png_clipart_car_car_white.gd
@@ -0,0 +1,11 @@
+extends "res://Car.gd"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("hello friends")
+#my cool comment
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
